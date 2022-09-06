c8884b3c4e4520918be1320576afdcb6eb30b6de
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-06 19:54:29 -0300

    Resolvendo operções aritmeticas com variáveis

---
+++ variavelOperadoresAritmetricos.js
@@ -0,0 +1,13 @@
+let x = 10, y = 2 // A vírgula é a mesma coisa se eu tivesse definido cada variável em uma linha
+
+let resultado = x + y;
+console.log(resultado);
+
+resultado = x - y;
+console.log(resultado);
+
+resultado = x * y;
+console.log(resultado);
+
+resultado = x / y;
+console.log(resultado);
