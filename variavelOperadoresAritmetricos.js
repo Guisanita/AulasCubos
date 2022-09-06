5cc694d9025af9b82729d0ec393a8b69b2c73f6d
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-06 20:08:59 -0300

    Exercício 3 da aula de variáveis (Concatenando Strings e Usando String Template

--- variavelOperadoresAritmetricos.js
+++ variavelOperadoresAritmetricos.js
@@ -1,13 +1,13 @@
 let x = 10, y = 2 // A vírgula é a mesma coisa se eu tivesse definido cada variável em uma linha
 
 let resultado = x + y;
-console.log(resultado);
+console.log(`A soma de ${x} e ${y} é ${resultado}`);
 
 resultado = x - y;
-console.log(resultado);
+console.log(`A subtração de ${x} menos ${y} é ${resultado}`);
 
 resultado = x * y;
-console.log(resultado);
+console.log(`A multiplicação de ${x} e ${y} é ${resultado}`);
 
 resultado = x / y;
-console.log(resultado);
+console.log(`A divisão de ${x} por ${y} é ${resultado}`);
