91d389964cd95299daabf5af20d8e29f8e1f0678
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-06 19:40:55 -0300

    Undefined

--- variavelTipos.js
+++ variavelTipos.js
@@ -2,3 +2,6 @@ let nome = "Gui"; // String - Pode usar "", '' ou `´(que pode ainda usar String
 let idade = 30; // Number
 let altura = 1.64; // Number (sempre usar . e não vírgula)
 
+let nota // Undefined (pode somente colocar assim ou colocar = e não ter nada)
+
+console.log(nota);
