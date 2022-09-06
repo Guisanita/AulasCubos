25f06ca9f40f5f21c6f7f0953ea0f8a9a6dd2656
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-06 19:42:09 -0300

    Definindo valor

--- variavelTipos.js
+++ variavelTipos.js
@@ -5,3 +5,7 @@ let altura = 1.64; // Number (sempre usar . e não vírgula)
 let nota // Undefined (pode somente colocar assim ou colocar = e não ter nada)
 
 console.log(nota);
+
+nota = 10 // Definindo valor 
+
+console.log(nota);
