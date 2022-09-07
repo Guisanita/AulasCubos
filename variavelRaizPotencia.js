9f813fc71b005b7f3245f2cbeab2ccb384538ccc
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-06 22:05:39 -0300

    Apenas deixando as três formas testadas anteriormente

--- variavelRaizPotencia.js
+++ variavelRaizPotencia.js
@@ -2,7 +2,10 @@ let raiz = Math.sqrt(15);
 
 console.log(raiz);
 
-// let potencia = Math.pow(2, 3);
-let potencia = 2 ** 3;
+let potencia = Math.pow(2, 3);
 
 console.log(potencia);
+
+let potencia2 = 2 ** 3;
+
+console.log(potencia2);
